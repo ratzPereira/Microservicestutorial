95ab08fa8772aafb7616f6a72eeec6d78ef447a1
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-09-26 12:27:15 +0000

    added notification service, created the consumer, added needed configurations and testes in postman

---
+++ notification-service/src/main/java/com/ratz/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,26 @@
+package com.ratz.notificationservice;
+
+import com.ratz.notificationservice.event.OrderPlacedEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@SpringBootApplication
+@Slf4j
+@EnableEurekaClient
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+	@KafkaListener(topics = {"notificationTopic"})
+	public void handleNotification(OrderPlacedEvent orderPlacedEvent){
+
+		//send out an email
+
+		log.info("Notification sent for order {}" , orderPlacedEvent.getOrderNumber());
+	}
+}
